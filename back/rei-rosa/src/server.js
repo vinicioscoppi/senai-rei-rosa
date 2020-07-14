d9607dc8614f89ea6cce6a33bbfa7247be7db94a
AlexTheurer <alex.tm@hotmail.com>
2020-07-13 22:12:26 -0300

    Criação de um endpoint para adicionar os usuários dentro das salas

--- back/rei-rosa/src/server.js
+++ back/rei-rosa/src/server.js
@@ -7,6 +7,8 @@ const config = new Config(redis.createClient());
 const app = config.express();
 
 const NUMBER_OF_ROOMS_IN_MEMORY = 10;
+const NUMBER_OF_MAX_USER_IN_ROOM = 4;
+const NUMBER_OF_MIN_USER_IN_ROOM = 2;
 
 app.listen(config.PORT, () => {
     console.log(`> Server running on port ${config.PORT}.`);
@@ -29,4 +31,30 @@ app.get('/room/:numberOfRooms', (req, res) => {
         new Room().getRoomsFromOneTo(numberOfRooms, config.dbClient)
             .then(rooms => res.send(rooms[0].map(room => JSON.parse(room))));
     }
+});
+
+app.get('/user/:nameUser/:idRoom', (req, res) => {
+
+    if (isNaN(req.params.idRoom))
+        res.status(400).send(`Id da sala não é um numero`);
+
+    if (req.params.idRoom > 10)
+        res.status(400).send(`Id da sala não existe`);
+
+    const user = req.params.nameUser;
+    const idRoom = "room-" + req.params.idRoom;
+
+    new Room().getRoom(config.dbClient, idRoom).then(
+        room => {
+            var obj = JSON.parse(room);
+
+            if (obj._players.length >= NUMBER_OF_MAX_USER_IN_ROOM)
+                res.status(400).send(`Numero de usuários deve ser entre ${NUMBER_OF_MIN_USER_IN_ROOM} e ${NUMBER_OF_MAX_USER_IN_ROOM}.`);
+            else {
+                obj._players.push(user);
+
+                new Room().addToRoom(idRoom, config.dbClient, obj).then(ret => res.send("Done!"));
+            }
+        }
+    ).catch(error => console.error(error));
 });
