f7c933b1534c4762b229fe292ab82e3a1c6a38ec
gabizllima <lima.gabriela@totvs.com.br>
2020-08-13 20:23:05 -0300

    criação base roleta

---
+++ mobile/rei-rosa/src/screens/Roulette/View/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { TeamAtributes } from './../../../components/TeamAtribute/index'
+export default class Roulette extends Component {
+    render() {
+        return (
+            <TeamAtributes>
+            </TeamAtributes>
+        );
+    }
+}
