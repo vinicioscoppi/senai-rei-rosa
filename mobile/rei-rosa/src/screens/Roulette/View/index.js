cda9efc3a259eee934c1492a44ece1bc8a4d47ca
gabizllima <lima.gabriela@totvs.com.br>
2020-08-13 21:18:06 -0300

    implementando exemplo de roleta com iframe e webview

--- mobile/rei-rosa/src/screens/Roulette/View/index.js
+++ mobile/rei-rosa/src/screens/Roulette/View/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { TeamAtributes } from './../../../components/TeamAtribute/index'
+import { WebView } from 'react-native-webview';
 export default class Roulette extends Component {
     render() {
         return (
-            <TeamAtributes>
-            </TeamAtributes>
+            <WebView
+                source={{ html: '<iframe src="https://wheeldecide.com/e.php?c1=1&c2=2&c3=3&c4=4&c5=5&c6=6&cols=ff0000,0000ff&t=Roleta&time=5&tcol=fbfd00&width=300" width="500" height="500" scrolling="no" frameborder="0"></iframe>' }}
+                style={{ marginTop: 20 }}
+            />
         );
     }
 }
