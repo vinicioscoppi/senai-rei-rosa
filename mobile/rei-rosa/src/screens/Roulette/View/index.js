44655c0c86bc3083a0995af435d4b30c617e1608
gabizllima <lima.gabriela@totvs.com.br>
2020-08-24 19:08:00 -0300

    alterações roleta

--- mobile/rei-rosa/src/screens/Roulette/View/index.js
+++ mobile/rei-rosa/src/screens/Roulette/View/index.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 import { WebView } from 'react-native-webview';
+
 export default class Roulette extends Component {
-    render() {
-        return (
-            <WebView
-                source={{ html: '<iframe src="https://wheeldecide.com/e.php?c1=1&c2=2&c3=3&c4=4&c5=5&c6=6&cols=ff0000,0000ff&t=Roleta&time=5&tcol=fbfd00&width=300" width="500" height="500" scrolling="no" frameborder="0"></iframe>' }}
-                style={{ marginTop: 20 }}
-            />
-        );
-    }
+  injectjs() {
+    let jsCode = 'alert(hello)';
+    return jsCode;
+  }
+
+  render() {
+    return (
+      <WebView 
+        ref={'webview'} 
+        automaticallyAdjustContentInsets={false} 
+        javaScriptEnabled={true}
+        injectedJavaScript={this.injectjs()} 
+        source={ 
+          require('../../../assets/roullete.html') 
+        } 
+      />
+    );
+  }
 }
+/*       
+`<iframe
+    src="https://wheeldecide.com/e.php?c1=1&c2=2&c3=3&c4=4&c5=5&c6=6&cols=ff0000,0000ff&t=Roleta&time=5&tcol=fbfd00&width=300"
+    width="500"
+    height="500"
+    scrolling="no"
+    frameborder="0"
+></iframe>`
+
+//
+
+<ScrollView style={{ flex: 1 }}>
+    <HTML
+        html={htmlContent}
+        imagesMaxWidth={Dimensions.get('window').width}
+    />
+</ScrollView>
+*/
